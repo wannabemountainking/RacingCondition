b425426a1f2cc772cacf26f2024f409efe84beae
wannabemountainking <kdy0096@gmail.com>
2024-12-20 10:46:41 +0900

    Initial Commit

---
+++ RacingCondition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RacingCondition
+//
+//  Created by YoonieMac on 12/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
